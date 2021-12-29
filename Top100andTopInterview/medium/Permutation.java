bc53d3758b6f554b889e86812263228cebd781a0
dahuin-1 <dawhite01@naver.com>
2021-12-29 21:07:55 +0900

    add Permutation

--- Top100andTopInterview/medium/Permutation.java
+++ Top100andTopInterview/medium/Permutation.java
@@ -1,7 +1,26 @@
 package Top100andTopInterview.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permutation {
-    public static void main(String[] args) {
-//
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        // Arrays.sort(nums); // not necessary
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for(int i = 0; i < nums.length; i++){
+                if(tempList.contains(nums[i])) continue;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
     }
 }
